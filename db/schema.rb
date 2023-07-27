088d1321ee8ec82858c029784b3c60fcc2a5c410
0718hyui@gmail.com <0718hyui@gmail.com>
2023-07-27 12:28:29 +0900

    migrationファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_27_023618) do
+ActiveRecord::Schema.define(version: 2023_07_27_031045) do
 
   create_table "dashboards", force: :cascade do |t|
     t.integer "user_id"
