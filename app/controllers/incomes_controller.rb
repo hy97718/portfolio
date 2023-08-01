99e93d37e61bffb7f0c6c12cee1125468b3225e9
0718hyui@gmail.com <0718hyui@gmail.com>
2023-08-01 13:43:16 +0900

    herokuエラー解消のため,グラフ表示のコードを変更

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -3,6 +3,7 @@ class IncomesController < ApplicationController
     @location = Location.find(params[:location_id])
     if @location.user == current_user
       @incomes = @location.incomes.includes(:user).order(income_day: :desc, income_name: :asc)
+      @income_counts = @incomes.group(:income_name).count
     else
       flash[:alert] = "不正なアクセスです"
       redirect_to locations_path
